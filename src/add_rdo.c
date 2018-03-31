a53ba3566e92dca09eaf68b35c0dd5046540b8f8
George0112 <youmaker12345@gmail.com>
2018-03-31 22:07:46 +0800

    calculate ratio

--- src/add_rdo.c
+++ src/add_rdo.c
@@ -1,36 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+#define FRAME_SIZE 10000
 
 int main(int argc, char **argv){
 
-    if(argc < 3){
-        fprintf(stderr, "Usage: add_rdo <input> <output>\n");
+    if(argc < 4){
+        fprintf(stderr, "Usage: add_rdo <encodeOutput> <input> <output>\n");
         exit(1);
     }
 
-    FILE *input, *output;
+    FILE *encode ,*input, *output;
     char buffer[500];
+	printf("%s\n", argv[1]);
 
-    input = fopen(argv[1], "r");
+	encode = fopen(argv[1], "r");
+	if(!encode)
+		fprintf(stderr, "%s doesn't exist!\n", argv[1]);
+
+    input = fopen(argv[2], "r");
     if(!input){
-        fprintf(stderr, "%s doesn't exist!\n", argv[1]);
+        fprintf(stderr, "%s doesn't exist!\n", argv[2]);
         exit(1);
     }
 
-    output = fopen(argv[2], "w");
+    output = fopen(argv[3], "w");
     if(!output){
-        fprintf(stderr, "Open %s error!\n", argv[2]);
+        fprintf(stderr, "Open %s error!\n", argv[3]);
         exit(1);
     }
 
-    char check[2];
+    char check[2], par[20];
+
+	int i, j, level, size[FRAME_SIZE];
+	double Y[FRAME_SIZE], nouse, difference;
+
+	for(i = 0; i < 3; i++)
+		fgets(buffer, 100, encode);
+
+	i = 0;
+
+	while(fscanf(encode, "%s",par) != EOF){
+		if(!strcmp(par, "Y")){
+			fscanf(encode, "%lf", &Y[i]);
+		}else if(!strcmp(par,"Level")){
+			fscanf(encode, "%d", &level);
+			printf(" level = %d\n", level);
+		}else if(!strcmp(par, "V")){
+			fscanf(encode, "%lf", &nouse);
+			fscanf(encode, "%d", &size[i]);
+			printf(" i = %d, Y[i] = %lf, Y[i-1] = %lf, PSNR Difference = %lf, ratio = %lf\n",
+				 i, Y[i], i%4==0?0:Y[i-1], Y[i]-(((i%4)==0)?0:Y[i-1]), (Y[i]-(((i%4)==0)?0:Y[i-1]))/size[i]*1000000);
+			i++;
+		}
+	}
+
+	for(i = 0; i < 9; i++){
+		fgets(buffer, 100, input);
+		fgets(buffer, 100, input);
+	}
+
+	i = 0; j = 0;
 
     while(fgets(buffer, 100, input)){
-        int last = strlen(buffer);
-        buffer[last] = 0;
-        fprintf(output, "%s %d\n", buffer, rand()%4);
+		fprintf(output, "%s 0\n", buffer);
 		fgets(buffer, 100, input);
+		for(j = 0; j < 4; j++){
+			fgets(buffer, 100, input);
+			int last = strlen(buffer);
+			buffer[last] = 0;
+			fprintf(output, "%s %lf\n", buffer, (Y[i]-(((i%4)==0)?0:Y[i-1]))/size[i]*1000000);
+			fgets(buffer, 100, input);
+			i++;
+		}
     }
 
     fclose(input);
